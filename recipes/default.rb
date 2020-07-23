edf4d732e2d678069493f02cd7d8a0fe217b736d
Pradeep Joshi <pradeepjoshi@Pradeeps-MacBook-Pro.local>
2020-07-23 13:33:32 -0700

    Initial commit

---
+++ recipes/default.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook Name:: cat_nginx
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package 'nginx' do
+action :install
+end
+
+service 'nginx' do
+action [ :enable, :start ]
+end
